39eb49faf5db20a91bc9f200795ca318c36159c3
Alexander Gonzalez <alexfertel97@gmail.com>
2019-06-12 13:09:41 -0400

    Update coolc.sh

--- src/coolc.sh
+++ src/coolc.sh
@@ -3,9 +3,11 @@
 INPUT_FILE=$1
 OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 
+python3 main.py $INPUT_FILE $OUTPUT_FILE
+
 # Si su compilador no lo hace ya, aquí puede imprimir la información de contacto
-echo "LINEA_CON_NOMBRE_Y_VERSION_DEL_COMPILADOR"   # Recuerde cambiar estas
-echo "Copyright (c) 2019: Nombre1, Nombre2, Nombre3"    # líneas a los valores correctos
+echo "coolc v1.0"   # Recuerde cambiar estas
+echo "Copyright (c) 2019: Alexander A. Gonzalez Fertel, Leonardo Fleitas, Marcos Toranzo"    # líneas a los valores correctos
 
 # Llamar al compilador
 # echo "Compiling $INPUT_FILE into $OUTPUT_FILE"
